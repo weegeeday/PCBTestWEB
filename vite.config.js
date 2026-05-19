3007e1b3b716f83b7b613f40d2257a04c9e5311e
weegeeday <38634611+weegeeday@users.noreply.github.com>
2026-05-19 12:26:09 -0400

    fixcld

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(process.cwd(), 'src')
     },
   },
   server: {
